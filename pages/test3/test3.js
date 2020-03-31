bf7074f8d4a70017dbd5cf79fcf802ba316f22f4
15701570017@163.com <gaoyuanyuan9511@duia.com>
2020-03-31 15:02:00 +0800

    设置字符指定长度 多余...

--- pages/test3/test3.js
+++ pages/test3/test3.js
@@ -12,9 +12,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    console.log(this.getTitle())
   },
-
+  getTitle: function () {
+    if (this.GetLength(this.data.title) > 8) {
+      var label = this.cutstr(this.data.title, 6)
+      return label
+    } else {
+      return this.data.title
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -62,5 +69,42 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  // 获取字符串长度，汉字算2个，字母数字算一个
+  GetLength(val) {
+    var len = 0;
+    for (var i = 0; i < val.length; i++) {
+      var a = val.charAt(i);
+      if (a.match(/[^\x00-\xff]/ig) != null) {
+        len += 2;
+      }
+      else {
+        len += 1;
+      }
+    }
+    return len;
+  },
+  cutstr(str, len) {
+    var str_length = 0;
+    var str_len = 0;
+    var str_cut = new String();
+    str_len = str.length;
+    for (var i = 0; i < str_len; i++) {
+      var a = str.charAt(i);
+      str_length++;
+      if (escape(a).length > 4) {
+        //中文字符的长度经编码之后大于4  
+        str_length++;
+      }
+      str_cut = str_cut.concat(a);
+      if (str_length >= len) {
+        str_cut = str_cut.concat("...");
+        return str_cut;
+      }
+    }
+    //如果给定字符串小于指定长度，则返回源字符串；  
+    if (str_length < len) {
+      return str;
+    }
+  },
 })
